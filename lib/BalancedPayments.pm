50aea9820afe77be08cd73d415e268d8a8d737ce
Naveed Massjouni <naveedm9@gmail.com>
2012-06-08 04:16:17 -0400

    Added get_account, create_account, and add_card methods

--- lib/BalancedPayments.pm
+++ lib/BalancedPayments.pm
@@ -1,7 +1,11 @@
 package BalancedPayments;
 use Moo;
+use Data::Dump;
 
-use HTTP::Request::Common qw(GET POST);
+use Carp qw(croak);
+#use URI;
+#use HTTP::Request;
+use HTTP::Request::Common qw(GET POST PUT);
 use JSON qw(from_json);
 use LWP::UserAgent;
 
@@ -29,6 +33,11 @@ has cards_uri => (
     lazy => 1,
     default => sub { shift->marketplace->{cards_uri} }
 );
+has accounts_uri => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { shift->marketplace->{accounts_uri} }
+);
 
 sub get_card {
     my ($self, $id) = @_;
@@ -36,8 +45,35 @@ sub get_card {
 }
 
 sub create_card {
-    my ($self, $args) = @_;
-    return $self->_post($self->cards_uri, $args);
+    my ($self, $card) = @_;
+    croak 'The card param must be a hashref' unless ref $card eq 'HASH';
+    return $self->_post($self->cards_uri, $card);
+}
+
+sub get_account {
+    my ($self, $id) = @_;
+    return $self->_get($self->accounts_uri . "/$id");
+}
+
+sub create_account {
+    my ($self, $account, $card) = @_;
+    croak 'The account param must be a hashref' unless ref $account eq 'HASH';
+    croak 'The account requires an email_address field'
+        unless $account->{email_address};
+    if ($card) {
+        croak 'The card param must be a hashref' unless ref $card eq 'HASH';
+        croak 'The card is missing a uri' unless $card->{uri};
+        $account->{card_uri} = $card->{uri};
+    }
+    return $self->_post($self->accounts_uri, $account);
+}
+
+sub add_card {
+    my ($self, $card, $account) = @_;
+    croak 'The card param must be a hashref' unless ref $card eq 'HASH';
+    croak 'The account param must be a hashref' unless ref $account eq 'HASH';
+    croak 'The account requires a cards_uri field' unless $account->{cards_uri};
+    return $self->_post($account->{cards_uri}, $card);
 }
 
 sub _url { $_[0]->base_url . $_[1] }
@@ -63,6 +99,18 @@ sub _post {
     my ($self, $path, $params) = @_;
     return $self->_req(POST $path, content => $params);
 }
+
+#sub _put {
+#    my ($self, $path, $params) = @_;
+#    #dd "_put: ", $path, $params;
+#    my $url = URI->new('http:');
+#    $url->query_form(%$params);
+#    my $content = $url->query;
+#    my $req = HTTP::Request->new(PUT => $path,
+#        [ content_type => 'application/x-www-form-urlencoded'], $content);
+#    return $self->_req($req);
+#}
+
 
 sub _req {
     my ($self, $req) = @_;
@@ -117,11 +165,12 @@ L<BalancedPayments|https://www.balancedpayments.com> API.
 
     get_card($id)
 
-Returns a credit card hashref for the given id.
-Here is an example response:
+Returns a credit card for the given id.
+
+Example response:
 
     { 
-        account          => '123',
+        account          => { ... },
         brand            => "MasterCard",
         card_type        => "mastercard",
         created_at       => "2012-06-07T11:00:40.003671Z",
@@ -143,8 +192,72 @@ Here is an example response:
         security_code    => 123,
     })
 
-Creates a credit card and returns the corresponding hashref.
+Creates a credit card and returns the server's representation of it.
 See L</get_card> for an example response.
+
+=head2 get_account
+
+    get_account($id)
+
+Returns an account for the given id.
+
+Example response:
+
+ {
+     id                => "AC7A",
+     uri               => "/v1/marketplaces/MK98/accounts/AC7A",
+     email_address     => "naveed\@crowdtilt.com",
+     meta              => {},
+     name              => undef,
+     roles             => [],
+     created_at        => "2012-06-07T21:01:38.801460Z",
+     bank_accounts_uri => "/v1/marketplaces/MK98/accounts/AC7A/bank_accounts",
+     cards_uri         => "/v1/marketplaces/MK98/accounts/AC7A/cards",
+     credits_uri       => "/v1/marketplaces/MK98/accounts/AC7A/credits",
+     debits_uri        => "/v1/marketplaces/MK98/accounts/AC7A/debits",
+     holds_uri         => "/v1/marketplaces/MK98/accounts/AC7A/holds",
+     refunds_uri       => "/v1/marketplaces/MK98/accounts/AC7A/refunds",
+     transactions_uri  => "/v1/marketplaces/MK98/accounts/AC7A/transactions",
+ }
+
+=head2 create_account
+
+    create_account({ email_address => 'bob@crowdtilt.com' })
+    create_account({ email_address => 'bob@crowdtilt.com' }, $card)
+    create_account({
+        email_address => 'bob@crowdtilt.com',
+        card_uri => "/v1/marketplaces/MK98/cards/CC92QRQcwUCp5zpzEz7lXKS",
+    })
+
+Creates an account and returns the server's representation of it.
+An account hashref is required and an optional card hashref may be provided as
+well.
+The account hashref must provide an email_address field.
+It is possible to create an account and associate it with a credit card at the
+same time.
+You can do this in 2 ways.
+If you have a card hashref, such as one returned by calling L</get_card>,
+then you can:
+
+    create_account({ email_address => 'bob@crowdtilt.com' }, $card)
+
+Alternatively, you can provide a card_uri inside the C<$account> hashref:
+
+    $bp->create_account({
+        email_address => 'bob@crowdtilt.com',
+        card_uri      => $card->{uri},
+    })
+
+See L</get_account> for an example response.
+
+=head2 add_card
+
+    add_card($card, $account)
+
+Adds a card to an account and returns the account object.
+It expects a card hashref, such as one returned by L</get_card>,
+and an account hashref, such as one returned by L</get_account>.
+See L</get_account> for an example response.
 
 =cut
 
